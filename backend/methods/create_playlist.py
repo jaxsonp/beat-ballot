c52823710240bd2e4bd17caa941e523a94228113
Jaxson Pahukula <jaxpahu@gmail.com>
2024-01-20 14:31:29 -0500

    backend update

--- backend/methods/create_playlist.py
+++ backend/methods/create_playlist.py
@@ -6,10 +6,18 @@ def create_playlist(name: str) -> Response:
     if name == None:
         return make_response(jsonify({ "message": "No name provided" }), 400)
 
-    print(f"Creating playlist \"{name}\"")
+    print(f"Creating playlist \"{name}\" ", end="")
     connection = sqlite3.connect(os.environ["DB_PATH"])
     cursor = connection.cursor()
-    result = cursor.execute(f"INSERT INTO Playlists (PlaylistName)\nVALUES (\"{name}\");")
+
+    # verifying user
+    # user_id = cursor.execute(f"SELECT UserID FROM Sessions WHERE Token=\"{name}\";").fetchone()[0]
+
+    cursor.execute(f"INSERT INTO Playlists (PlaylistName)\nVALUES (\"{name}\");")
+    #playlist_id = cursor.execute(f"SELECT PlaylistID FROM Playlists WHERE PlaylistName=\"{name}\";").fetchone()[0]
+    #cursor.execute(f"INSERT INTO UserPlaylist (PlaylistName)\nVALUES (\"{name}\");")
+    #print(f"success (id {playlist_id})")
+    print("success")
     connection.commit()
     connection.close()
 
