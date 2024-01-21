32ecc347a4285bdb1949f79917707f8333e61b56
joylynpan <pan379@purdue.edu>
2024-01-21 07:27:40 -0500

    landing

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -8,7 +8,7 @@ import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Paper from "@mui/material/Paper";
-import { CardActionArea } from "@mui/material";
+import { CardActionArea, TextField } from "@mui/material";
 import Menu from "@mui/material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
@@ -31,12 +31,18 @@ export default function Landing() {
 
         <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
             <Paper style={{ padding: "2rem", margin: "2rem" }}>
-                <div style={{ display: "flex", marginBottom: "2rem" }}>
-                    <img src="/icon512.png" style={{ width: "10rem", marginRight: "3rem" }} alt="large icon" />
-                    <Typography variant="h1" style={{ flexGrow: 1, fontFamily: "monospace", letterSpacing: ".3rem", marginTop: "3rem" }}>
-                        BeatBallot
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <div style={{ display: "flex", marginBottom: "2rem" }}>
+                        <img src="/icon512.png" style={{ width: "10rem", marginRight: "3rem" }} alt="large icon" />
+                        <Typography variant="h1" style={{ flexGrow: 1, fontFamily: "monospace", letterSpacing: ".3rem", marginTop: "3rem" }}>
+                            BeatBallot
+                        </Typography>
+                    </div>
+                    <Typography variant="body1" style={{marginBottom: "1rem", textAlign: "left", color: "white"}}>
+                        hello
                     </Typography>
-                </div>
+                </div> 
+            
                 <div style={{ display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "flex-end", gap: "1rem" }}>
                     <Link to="/sign-in">
                         <Button style={{ backgroundColor: "#29B77E", color: "white", fontFamily: "monospace", width: "120px", height: "40px" }} variant="contained">Sign In</Button>
