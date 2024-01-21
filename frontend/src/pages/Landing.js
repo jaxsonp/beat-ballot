c35ccff79425d11fdae5c794b7579451052a0e24
joylynpan <pan379@purdue.edu>
2024-01-21 07:14:45 -0500

    updated landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -15,18 +15,36 @@ import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import { Button } from "@mui/material";
+import { createTheme } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom';
+
+const theme = createTheme({
+    palette: {
+        loudgreen: '#52E354',
+    },
+  });
+
 
 export default function Landing() {
     return (
+
         <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
             <Paper style={{ padding: "2rem", margin: "2rem" }}>
                 <div style={{ display: "flex", marginBottom: "2rem" }}>
                     <img src="/icon512.png" style={{ width: "10rem", marginRight: "3rem" }} alt="large icon" />
-                    <Typography variant="h1" style={{ flexGrow: 1, marginTop: "3rem" }}>
+                    <Typography variant="h1" style={{ flexGrow: 1, fontFamily: "monospace", letterSpacing: ".3rem", marginTop: "3rem" }}>
                         BeatBallot
                     </Typography>
                 </div>
-                <Button>Sign in</Button>
+                <div style={{ display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "flex-end", gap: "1rem" }}>
+                    <Link to="/sign-in">
+                        <Button style={{ backgroundColor: "#29B77E", color: "white", fontFamily: "monospace", width: "120px", height: "40px" }} variant="contained">Sign In</Button>
+                    </Link>
+                    <Link to="/sign-up">
+                        <Button style={{ backgroundColor: "#29B77E", color: "white", fontFamily: "monospace", width: "120px", height: "40px" }} variant="contained">Sign Up</Button>
+                    </Link>
+                </div>
                 <hr></hr>
             </Paper>
         </div>
